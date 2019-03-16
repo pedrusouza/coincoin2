3f71edd83fd4c39537678775b281434ee7bf8f1f
Diego Oliveira <diegommobr@gmail.com>
2019-03-16 17:39:02 -0300

    Referindo a um mesmo arquivo de delete

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -13,7 +13,7 @@ $('#delete-modal-logistas').on('show.bs.modal', function (event) {
   var id = button.data('usuario');
   var modal = $(this);
   modal.find('.modal-title').text('Excluir Usuário com o ID ' + id);	  
-  modal.find('#confirm').attr('href', 'deletelogista.php?id=' + id);	
+  modal.find('#confirm').attr('href', 'delete.php?id=' + id);	
 })
 
 $('#insert-modal').on('show.bs.modal', function (event) {
